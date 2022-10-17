ef554862ccacd281eb5c7d17c3c66960911c6811
Dmitry <ryabchikk@rambler.ru>
2022-10-17 19:48:01 +0300

    day2

--- SF Module 7 HW03/SF Module 7 HW03/Program.cs
+++ SF Module 7 HW03/SF Module 7 HW03/Program.cs
@@ -1,16 +1,33 @@
 ﻿using System;
 using System.Net;
+using System.Runtime.CompilerServices;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-
         DateOnly dateOnly = new DateOnly(2022, 6, 12);
         string str = "jjj";
 
-        ShopDelivery ShopDelivery = new ShopDelivery(dateOnly, str);
+        ShopDelivery shopDelivery = new ShopDelivery(str);
+
+        Product a1 = new Product("A1", "B1");
+        Product a2 = new Product("A2", "B2");
+
+        Product[] products = { a1, a2 };
+
+
+
+
+        Order<ShopDelivery> order = new Order<ShopDelivery>(shopDelivery, (555, "ЕЦ"), products, ("Pup", "pip", "999-555-888", "@mail.ru"));
+
+
+
+        order.DisplayOrderInfo();
+        order.DisplayAddress();
+        order.DisplayProductList();
+        order.DisplayUserInfo();
+
 
         Console.ReadKey();
 
@@ -21,10 +38,9 @@ internal class Program
 abstract class Delivery
 {
     public string Address;
-    public DateOnly Date;
-    public Delivery(DateOnly date, string address)
+
+    public Delivery(string address)
     {
-        Date = date;
         Address = address;
     }
 }
@@ -35,37 +51,76 @@ class HomeDelivery : Delivery
     /// <summary>
     /// Примерное время доставки
     /// </summary>
-    public TimeOnly Time { get; set; }
+    public DateTime DeliveryDateTime { get; set; }
 
-    public HomeDelivery(DateOnly date, TimeOnly time, string address) : base(date, address)
+    public HomeDelivery(DateTime deliveryDateTime, string address) : base(address)
     {
-        Time = time;
+        DeliveryDateTime = deliveryDateTime;
     }
 }
 
 class PickPointDelivery : Delivery
 {
     /* ... */
-    public PickPointDelivery(DateOnly date, string address) : base(date, address)
-    { }
+    public DateOnly bookingDeadline { get; }
+    public PickPointDelivery(string address) : base(address)
+    {
+        DateTime actualDateTime = DateTime.Now;
+        DateOnly actualDate = new DateOnly(actualDateTime.Year, actualDateTime.Month, actualDateTime.Day);
+        bookingDeadline = actualDate.AddDays(10);
+    }
 }
 
 class ShopDelivery : Delivery
 {
     /* ... */
-
-    public ShopDelivery(DateOnly date, string address) : base(date, address)
-    { }
+    public DateOnly bookingDeadline { get; }
+    public ShopDelivery(string address) : base(address)
+    {
+        DateTime actualDateTime = DateTime.Now;
+        DateOnly actualDate = new DateOnly(actualDateTime.Year, actualDateTime.Month, actualDateTime.Day);
+        bookingDeadline = actualDate.AddDays(10);
+    }
 }
+
+
+static class DeliveryExtensions
+{
+    /// <summary>
+    /// Проверка срока хранения
+    /// </summary>
+    /// <param name="delivery"></param>
+    /// <returns></returns>
+    public static bool DeliveryBookingExpired(this PickPointDelivery delivery)
+    {
+        DateTime actualDateTime = DateTime.Now;
+        DateOnly actualDate = new DateOnly(actualDateTime.Year, actualDateTime.Month, actualDateTime.Day);
+        bool result = actualDate > delivery.bookingDeadline;
+        return result;
+    }
+    /// <summary>
+    /// Перегрузка метода Проверки срока хранения
+    /// </summary>
+    /// <param name="delivery"></param>
+    /// <returns></returns>
+    public static bool DeliveryBookingExpired(this ShopDelivery delivery)
+    {
+        DateTime actualDateTime = DateTime.Now;
+        DateOnly actualDate = new DateOnly(actualDateTime.Year, actualDateTime.Month, actualDateTime.Day);
+        bool result = actualDate > delivery.bookingDeadline;
+        return result;
+    }
+}
+
 
 /// <summary>
 /// Абстрактный класс информации
 /// </summary>
 abstract class Information<T1>
 {
-    private T1 Name { get; }
+    public T1 Name { get; }
 
-    private string Description { get; }
+    public string Description { get; }
 
     public Information(T1 name, string description)
     {
@@ -84,9 +139,13 @@ class Product : Information<string>
     }
 }
 
+
+/// <summary>
+/// Список заказа
+/// </summary>
 class ProductList
 {
-    private Product[] Items;
+    private protected Product[] Items;
     public Product this[int index]
     {
         get
@@ -109,15 +168,20 @@ class ProductList
         }
     }
 
+    public int getCount()
+    {
+        return Items.Length;
+    }
+
     public ProductList(Product[] products)
     {
-        if (products != null)
-        {
-            Items = products;
-        }
+        Items = products;
     }
 }
 
+/// <summary>
+/// Информация о заказе-Идентификатор
+/// </summary>
 class OrderInformation : Information<int>
 {
     public OrderInformation(int number, string description) : base(number, description)
@@ -130,10 +194,30 @@ class OrderInformation : Information<int>
 /// </summary>
 class CustomerInformation
 {
-    private string firstName { get; }
-    private string lastName { get; }
-    private string phoneNumber { get; set; }
-    private string email { get; set; }
+    protected string firstName { get; }
+    protected string lastName { get; }
+    protected string phoneNumber { get; set; }
+    protected string email { get; set; }
+
+    public string GetCustomerFirstName()
+    {
+        return firstName;
+    }
+
+    public string GetCustomerLastName()
+    {
+        return lastName;
+    }
+
+    public string GetCustomerPhoneNumber()
+    {
+        return phoneNumber;
+    }
+
+    public string GetCustomerEmail()
+    {
+        return email;
+    }
 
     public CustomerInformation(string firstName, string lastName, string phoneNumber, string email)
     {
@@ -143,7 +227,7 @@ class CustomerInformation
         this.email = email;
     }
 }
-class Order<TDelivery, TStruct> where TDelivery : Delivery
+class Order<TDelivery> where TDelivery : Delivery
 {
     public TDelivery Delivery;
 
@@ -158,13 +242,50 @@ class Order<TDelivery, TStruct> where TDelivery : Delivery
     public CustomerInformation Customer;
 
 
+
     public void DisplayAddress()
     {
-        Console.WriteLine(Delivery.Address);
+        Console.WriteLine("Адрес: {0}", Delivery.Address);
     }
 
+    public void DisplayUserInfo()
+    {
+        if (Customer != null)
+        {
+            Console.WriteLine("Информация о заказчике: \n\t Имя:{0} \n\t Фамилия:{1} \n\t Телефон:{2} \n\tЭлектронная почта:{3}",
+            Customer.GetCustomerFirstName(), Customer.GetCustomerLastName(), Customer.GetCustomerPhoneNumber(), Customer.GetCustomerEmail());
+        }
+    }
 
+    public void DisplayProductList()
+    {
+        if (ProductList != null)
+        {
+            Console.WriteLine("Список заказа:");
+            for (int i = 0; i < ProductList.getCount(); i++)
+            {
+                Product product = ProductList[i];
+                Console.WriteLine("{0}.\t {1}.", i + 1, product.Name);
+            }
+        }
+        else { Console.WriteLine("Список заказа отсутствует"); }
+    }
 
+    public void DisplayOrderInfo()
+    {
+        if (OrderInformation != null)
+        {
+            Console.WriteLine("Номер заказа:\t{0}. \nОписание заказа: \t{1}", OrderInformation.Name, OrderInformation.Description);
+        }
+    }
 
-    // ... Другие поля
+    public Order(TDelivery delivery, (int number, string description) orderInformation, Product[] products,
+        (string firstName, string lastName, string phoneNumber, string email) UserInformation)
+    {
+        Delivery = delivery;
+        OrderInformation = new OrderInformation(orderInformation.number, orderInformation.description);
+        ProductList = new ProductList(products);
+        Customer = new CustomerInformation(UserInformation.firstName, UserInformation.lastName, UserInformation.phoneNumber, UserInformation.email);
+    }
+
 }
